94be334fcca46ed89ec83bc0517367b1dc7bbef8
Wayne Williams <104110823+wayne6092@users.noreply.github.com>
2022-07-20 00:27:56 -0400

    Ping Pong 2 Modifications

---
+++ Ping Pong 2/pong3.2DoingGreat.py
@@ -0,0 +1,256 @@
+import pygame
+
+
+SCR_WID, SCR_HEI = 640, 480
+class Player():
+        def __init__(self, name):
+                self.name = name
+                if self.name == "hero":
+                        self.x, self.y = 16, SCR_HEI/2
+                        self.padWid, self.padHei = 8, 64
+                        
+                elif self.name == "enemy":
+                        self.padWid, self.padHei = 8, 64
+                        self.x, self.y = SCR_WID-16, SCR_HEI/2
+
+                   
+       
+                else:
+                        self.y, self.x = SCR_WID-400, SCR_HEI-30
+                        self.padWid, self.padHei = 64, 8
+                        
+                self.speed = 3
+                self.score = 0
+                self.scoreFont = pygame.font.Font("imagine_font.ttf", 64)
+       
+        def scoring(self):
+                scoreBlit = self.scoreFont.render(str(self.score), 1, (255, 255, 255))
+                if self.name == "hero":
+                    screen.blit(scoreBlit, (32, 16))
+                    if self.score == 10:
+                        print ("player 1 wins!")
+                        exit()
+                elif self.name == "enemy":
+                    screen.blit(scoreBlit, (SCR_HEI+92, 16))
+                    if self.score == 10:
+                        print ("player 2 wins!")
+                        exit()
+
+                else:
+                    screen.blit(scoreBlit, (SCR_HEI/2, SCR_WID/2))
+                    if self.score == 10:
+                        print ("player 3 wins!")
+                        exit()
+   
+       
+        def movement(self):
+                keys = pygame.key.get_pressed()
+                if self.name == "hero":
+                    if keys[pygame.K_w]:
+                            self.y -= self.speed
+                    elif keys[pygame.K_s]:
+                            self.y += self.speed
+           
+                    if self.y <= 0:
+                            self.y = 0
+                    elif self.y >= SCR_HEI-64:
+                            self.y = SCR_HEI-64
+
+                elif self.name == "enemy":
+                    if keys[pygame.K_UP]:
+                            self.y -= self.speed
+                    elif keys[pygame.K_DOWN]:
+                            self.y += self.speed
+           
+                    if self.y <= 0:
+                            self.y = 0
+                    elif self.y >= SCR_HEI-64:
+                            self.y = SCR_HEI-64
+                            
+                else:
+                    if keys[pygame.K_f]:
+                        self.y -= self.speed#left
+                    elif keys[pygame.K_g]:
+                        self.y += self.speed
+
+                    if self.y <= 0:
+                        self.y = 0
+                    elif self.y > SCR_WID-64:
+                        self.y = SCR_WID-64
+        def draw(self):
+                pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.padWid, self.padHei))
+
+        def draw2(self):
+                pygame.draw.rect(screen, (255, 255, 255), ( self.y, self.x, self.padWid, self.padHei))
+##class Enemy():
+##        def __init__(self):
+##                self.x, self.y = SCR_WID-16, SCR_HEI/2
+##                self.speed = 3
+##                self.padWid, self.padHei = 8, 64
+##                self.score = 0
+##                self.scoreFont = pygame.font.Font("imagine_font.ttf", 64)
+##       
+##        def scoring(self):
+##                scoreBlit = self.scoreFont.render(str(self.score), 1, (255, 255, 255))
+##                screen.blit(scoreBlit, (SCR_HEI+92, 16))
+##                if self.score == 10:
+##                        print ("Player 2 wins!")
+##                        exit()
+##       
+##        def movement(self):
+
+##       
+##        def draw(self):
+##                pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.padWid, self.padHei))
+## 
+class Ball():
+        def __init__(self):
+                self.x, self.y = SCR_WID/2, SCR_HEI/2
+                self.speed_x = -3
+                self.speed_y = 3
+                self.size = 10
+
+                self.x, self.y = SCR_WID/2, SCR_HEI/2
+                self.speed_x = -3
+                self.speed_y = 3
+                self.size = 10 
+        def movement(self):
+                self.x += self.speed_x
+                self.y += self.speed_y
+                
+                #wall collide bottom top
+
+                if self.y <= 0:
+                        self.speed_y *= -1
+                  
+                elif self.y >= SCR_HEI-self.size:
+                        self.__init__()
+                        player2.score -= 1
+                        
+                if self.x <= 0:
+                        self.__init__()
+                        enemy.score += 1
+                        #ballOut.play()
+                        
+                elif self.x >= SCR_WID-self.size:
+                        self.__init__()
+                        self.speed_x = 3
+                        player.score += 1
+                        
+                ##wall col
+                #paddle col
+                #player
+                #                  y is width                 x is height
+                                                
+                for n in range(-self.size, player2.padWid):
+                        if self.y == player2.x + n:
+                                if self.x >= player2.padHei:
+                                        if self.x >= player2.y - player2.padWid:
+                                                if self.x <= player2.y + player2.padWid:
+                                                        self.speed_y *= -1
+                                                        ballOut.play()#Added sound to ball hit
+                                                        
+                                                        player2.score += 1
+       
+                                                        break
+                                                        #self.speed_y *= -1
+                                        
+
+                        n += 1
+                        
+                for n in range(-self.size, player.padHei):
+                        if self.y == player.y + n:
+                                if self.x <= player.x + player.padWid:
+                                        self.speed_x *= -1
+                                        ballOut.play()#Added sound to ball hit
+                                        break
+                        n += 1
+                #enemy
+                for n in range(-self.size, enemy.padHei):
+                        if self.y == enemy.y + n:
+                                if self.x >= enemy.x - enemy.padWid:
+                                        self.speed_x *= -1
+                                        ballOut.play()#Added sound to ball hit
+                                        break
+                        n += 1
+
+
+
+                ##paddle col
+ 
+        def draw(self):
+                pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 8, 8))
+
+                
+
+
+
+pygame.init()
+SCR_WID, SCR_HEI = 640, 480
+screen = pygame.display.set_mode((SCR_WID, SCR_HEI))
+pygame.display.set_caption("Pong")
+pygame.font.init()
+clock = pygame.time.Clock()
+FPS = 60
+pygame.mixer.music.load('pickup.wav')
+ballOut = pygame.mixer.Sound('pickup.wav')
+#pygame.mixer.music.play(-1, 0.0)
+musicPlaying = True
+backGround = pygame.image.load('pingpongtable.png')
+backGroundStretched = pygame.transform.scale(backGround, (640, 480))
+gameOver = pygame.image.load('gameover.png')
+
+
+player = Player("hero")
+player2 = Player("Jim")
+
+ball = Ball()
+#ball2 = Ball("2")
+#player = Player()
+enemy = Player("enemy")
+def main():
+
+        while True:
+            #process
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                            print ("Game exited by user")
+                            exit()
+
+
+                            
+            ##process
+            #logic
+            ball.movement()
+            player.movement()
+            enemy.movement()
+            player2.movement()
+            ##logic
+            #draw
+            screen.fill((0, 0, 0))
+            screen.blit(backGroundStretched, (0, 0))
+            ball.draw()
+            player.draw()
+            player2.draw2()
+            player2.scoring()
+            player.scoring()
+            enemy.draw()
+            enemy.scoring()
+##            if player2.score == 2:
+##                    ball2.movement()
+##                    ball2.draw()
+##                    
+            ##draw
+            #_______
+            pygame.display.flip()
+            clock.tick(FPS)
+
+main()
+
+
+#all the variables of Rect
+##x, y, top, left, bottom, right, topleft,
+##bottomleft, topright, bottomright, midtop,
+##midleft, midbottom, midright, center, centerx,
+##centery, size, width, height, w, h
+
---
+++ Ping Pong 2/pongplayers.py
@@ -0,0 +1,120 @@
+import pygame
+class Player():
+        def __init__(self):
+            
+                self.x, self.y = 16, SCR_HEI/2
+                self.speed = 3
+                self.padWid, self.padHei = 8, 64
+                self.score = 0
+                self.scoreFont = pygame.font.Font("imagine_font.ttf", 64)
+                
+
+                
+        def scoring(self):
+                scoreBlit = self.scoreFont.render(str(self.score), 1, (255, 255, 255))
+                screen.blit(scoreBlit, (32, 16))
+                if self.score == 10:
+                        print ("player 1 wins!")
+                        exit()
+        def scoring2(self):
+                scoreBlit = self.scoreFont.render(str(self.score), 1, (255, 255, 255))
+                screen.blit(scoreBlit, (SCR_HEI+92, 16))
+                if self.score == 10:
+                        print ("Player 2 wins!")
+                        exit()
+       
+
+       
+        def movement(self):
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_w]:
+                        self.y -= self.speed
+                elif keys[pygame.K_s]:
+                        self.y += self.speed
+       
+                if self.y <= 0:
+                        self.y = 0
+                elif self.y >= SCR_HEI-64:
+                        self.y = SCR_HEI-64
+
+                        
+        def movement2(self):
+
+                keys = pygame.key.get_pressed()
+                        
+                if keys[pygame.K_UP]:
+                        self.speed = 3
+                        self.y -= self.speed
+                    
+                elif keys[pygame.K_DOWN]:
+                        self.speed = 3
+                        self.y += self.speed
+
+
+                if self.y <= 0:
+                        self.y = 0
+                elif self.y >= SCR_HEI-64:
+                        self.y = SCR_HEI-64
+
+                elif player2:
+                    self.x = SCR_WID-16
+                    
+        def draw(self):
+                pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.padWid, self.padHei))
+                
+
+class Ball():
+        def __init__(self):
+                self.x, self.y = SCR_WID/2, SCR_HEI/2
+                self.speed_x = -3
+                self.speed_y = 3
+                self.size = 8
+       
+        def movement(self):
+                self.x += self.speed_x
+                self.y += self.speed_y
+ 
+                #wall col
+                if self.y <= 0:
+                        self.speed_y *= -1
+                elif self.y >= SCR_HEI-self.size:
+                        self.speed_y *= -1
+ 
+                if self.x <= 0:
+                        self.__init__()
+                        player2.score += 1
+                elif self.x >= SCR_WID-self.size:
+                        self.__init__()
+                        self.speed_x = 3
+                        player1.score += 1
+                ##wall col
+                #paddle col
+                #player1 ball dynamics
+                for n in range(-self.size, player1.padHei):
+                        if self.y == player1.y + n:
+                                if self.x <= player1.x + player1.padWid:
+                                        self.speed_x *= -1
+                                        break
+                        n += 1
+                #player2 ball dynamics
+                for n in range(-self.size, player2.padHei):
+                        if self.y == player2.y + n:
+                                if self.x >= player2.x - player2.padWid:
+                                        self.speed_x *= -1
+                                        break
+                        n += 1
+                ##paddle col
+ 
+        def drawBall(self):
+                pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 8, 8))
+                
+SCR_WID, SCR_HEI = 640, 480
+screen = pygame.display.set_mode((SCR_WID, SCR_HEI))
+pygame.display.set_caption("Pong")
+pygame.font.init()
+clock = pygame.time.Clock()
+FPS = 60
+
+player1 = Player()
+player2 = Player()
+ball = Ball()
